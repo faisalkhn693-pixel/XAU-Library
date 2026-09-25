8fed3932095ed0dffd587fe38d72206e3508f04f
faisalkhn693-pixel <faisal.khn693@gmail.com>
2026-09-25 18:22:20 +0500

    Use 5k CVD bucket intervals

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='xau-library-v4';
+const CACHE='xau-library-v5';
 const SHELL=['./','./index.html','./app.css','./app.js','./db.js','./case-model.js','./similarity.js','./research-engine.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
