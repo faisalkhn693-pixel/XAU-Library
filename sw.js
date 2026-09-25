50d674e1be338835c7e36af0b486cff91bd38486
faisalkhn693-pixel <faisal.khn693@gmail.com>
2026-09-25 20:23:11 +0500

    Keep positive and negative CVD buckets separate

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='xau-library-v6';
+const CACHE='xau-library-v7';
 const SHELL=['./','./index.html','./app.css','./app.js','./db.js','./case-model.js','./similarity.js','./research-engine.js','./trade-search.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
