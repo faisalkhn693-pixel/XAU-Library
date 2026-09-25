eb1c3b000e321eb9bca85142f480778a3ab75cf7
faisalkhn693-pixel <faisal.khn693@gmail.com>
2026-09-25 17:21:31 +0500

    Refresh service worker cache after storage hardening

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='xau-library-v1';
+const CACHE='xau-library-v2';
 const SHELL=['./','./index.html','./app.css','./app.js','./db.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
